6ce93f5c648158ab0c9c54c1bfee577d0d2a1edf
thiagovilarinholemes <lemes_vilarinho@yahoo.com.br>
2021-05-29 10:40:23 -0300

    Add configurations cors

---
+++ src/main/java/com/apimicroservice/CorsConfiguration.java
@@ -0,0 +1,19 @@
+package com.apimicroservice;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(true)
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .exposedHeaders(HttpHeaders.SET_COOKIE);
+    }
+}
